7f86e0e8e4261c33b4840a29a6538a1d56034fef
Ubuntu <ubuntu@mail.rechargedialysis.com>
2013-07-16 02:33:59 +0000

    Incorporated mocha testing framework and added login page scaffold

---
+++ merrill/tests/login.js
@@ -0,0 +1,18 @@
+var assert = require("assert");
+var chai = require("chai");
+var request = requre("supertest");
+request = request('http://rechargedialysis.com');
+
+chai.should();
+
+describe('Array', function () {
+		describe('#indexOf()', function() {
+				it('should return -1 when the value is not present', function() {
+						assert.equal(-1, [1,2,3].indexOf(5));
+						assert.equal(-1, [1,2,3].indexOf(0));
+						"me".should.be.a('string');
+				})
+		})
+});
+
+
